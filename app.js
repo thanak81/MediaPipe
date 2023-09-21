90f0da7b5a0f036450844818da5b94de9df950e1
Thanak Mech <thanakmech@gmail.com>
2023-09-21 14:36:41 +0700

    Styling done v0.02

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
-// Copyright 2023 The MediaPipe Authors.
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//      http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-
-// limitations under the License.
 import {
   ObjectDetector,
   FilesetResolver,
 } from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.2";
-const demosSection = document.getElementById("demos");
 let objectDetector;
 let runningMode = "IMAGE";
 // Initialize the object detector
@@ -30,92 +18,9 @@ const initializeObjectDetector = async () => {
     runningMode: runningMode,
     category_deny_list: ["person"],
   });
-  demosSection.classList.remove("invisible");
 };
 initializeObjectDetector();
-/********************************************************************
- // Demo 1: Grab a bunch of images from the page and detection them
- // upon click.
- ********************************************************************/
-const imageContainers = document.getElementsByClassName("detectOnClick");
-for (let imageContainer of imageContainers) {
-  imageContainer.children[0].addEventListener("click", handleClick);
-}
-/**
- * Detect objects in still images on click
- */
-async function handleClick(event) {
-  const highlighters =
-    event.target.parentNode.getElementsByClassName("highlighter");
-  while (highlighters[0]) {
-    highlighters[0].parentNode.removeChild(highlighters[0]);
-  }
-  const infos = event.target.parentNode.getElementsByClassName("info");
-  while (infos[0]) {
-    infos[0].parentNode.removeChild(infos[0]);
-  }
-  if (!objectDetector) {
-    alert("Object Detector is still loading. Please try again.");
-    return;
-  }
-  // if video mode is initialized, set runningMode to image
-  if (runningMode === "VIDEO") {
-    runningMode = "IMAGE";
-    await objectDetector.setOptions({ runningMode: "IMAGE" });
-  }
-  const ratio = event.target.height / event.target.naturalHeight;
-  // objectDetector.detect returns a promise which, when resolved, is an array of Detection objects
-  const detections = objectDetector.detect(event.target);
-  displayImageDetections(detections, event.target);
-  console.log(detections);
-}
-function displayImageDetections(result, resultElement) {
-  const ratio = resultElement.height / resultElement.naturalHeight;
-  console.log(ratio);
-  for (let detection of result.detections) {
-    // Description text
-    const p = document.createElement("p");
-    p.setAttribute("class", "info");
-    p.innerText =
-      detection.categories[0].categoryName +
-      " - with " +
-      Math.round(parseFloat(detection.categories[0].score) * 100) +
-      "% confidence.";
-    // Positioned at the top left of the bounding box.
-    // Height is whatever the text takes up.
-    // Width subtracts text padding in CSS so fits perfectly.
-    p.style =
-      "left: " +
-      detection.boundingBox.originX * ratio +
-      "px;" +
-      "top: " +
-      detection.boundingBox.originY * ratio +
-      "px; " +
-      "width: " +
-      (detection.boundingBox.width * ratio - 10) +
-      "px;";
-    const highlighter = document.createElement("div");
-    highlighter.setAttribute("class", "highlighter");
-    highlighter.style =
-      "left: " +
-      detection.boundingBox.originX * ratio +
-      "px;" +
-      "top: " +
-      detection.boundingBox.originY * ratio +
-      "px;" +
-      "width: " +
-      detection.boundingBox.width * ratio +
-      "px;" +
-      "height: " +
-      detection.boundingBox.height * ratio +
-      "px;";
-    resultElement.parentNode.appendChild(highlighter);
-    resultElement.parentNode.appendChild(p);
-  }
-}
-/********************************************************************
- // Demo 2: Continuously grab image from webcam stream and detect it.
- ********************************************************************/
+
 let video = document.getElementById("webcam");
 const liveView = document.getElementById("liveView");
 let enableWebcamButton;
@@ -156,9 +61,6 @@ async function enableCam(event) {
             facingMode
           },
     }
-
-
-  
   // Activate the webcam stream.
   try{
     if(stream){
@@ -170,7 +72,6 @@ async function enableCam(event) {
         console.error(err);
         /* handle the error */
       }};
-        video.srcObject = null;
         video.srcObject = stream;
         video.play();
         video.addEventListener("loadeddata", predictWebcam);
@@ -258,14 +159,13 @@ function displayVideoDetections(result) {
       "left: " +
       (video.offsetWidth -
         detection.boundingBox.width -
-        detection.boundingBox.originX +
-        180) +
+        detection.boundingBox.originX + 140) +
       "px;" +
       "top: " +
       detection.boundingBox.originY +
       "px; " +
       "width: " +
-      (detection.boundingBox.width - 1200) +
+      (detection.boundingBox.width - 50) +
       "px;";
     const highlighter = document.createElement("div");
     highlighter.setAttribute("class", "highlighter");
@@ -273,14 +173,13 @@ function displayVideoDetections(result) {
       "left: " +
       (video.offsetWidth -
         detection.boundingBox.width -
-        detection.boundingBox.originX +
-        180) +
+        detection.boundingBox.originX +140) +
       "px;" +
       "top: " +
       detection.boundingBox.originY +
       "px;" +
       "width: " +
-      (detection.boundingBox.width - 110) +
+      (detection.boundingBox.width-50) +
       "px;" +
       "height: " +
       detection.boundingBox.height +
@@ -292,12 +191,36 @@ function displayVideoDetections(result) {
     children.push(p);
   }
   let output="";
-        newArr.map((item) => (
-          output += `
-                ${item.categoryName}
-                ${item.content}
-                ${item.age}
-            `));
-        showResults.innerText = output
-    
+        newArr.map((item) => {
+          return output += `
+          <div class="results-card">
+                <div>Category: ${item.categoryName}</div>
+                <div>Description: ${item.content}</div>
+                <div>Age: ${item.age}</div>
+                <div>
+                  <span><a href="#">Facebook</a></span>
+                  <span><a href="#">Twitter</a></span>
+                  <span><a href="#">Instagram</a></span>
+                  <span><a href="#">Linkedin</a></span>
+                </div>
+          </div>`
+          });
+  showResults.innerHTML = output
+
 }
+
+// let v = document.getElementById("webcam");
+
+// v.addEventListener(
+//   "resize",
+//   (ev) => {
+//     let w = v.videoWidth;
+//     let h = v.videoHeight;
+
+//     if (w && h) {
+//       v.style.width = w;
+//       v.style.height = h;
+//     }
+//   },
+//   false,
+// );
